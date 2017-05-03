be19a1c9f00490bd9d9e509770364b3377501882
NawrasRabbani <nawras.rabbani@mail.mcgill.ca>
2017-05-03 09:38:53 -0400

    recorder works

--- Interviewer/urls.py
+++ Interviewer/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^recorder$', views.recorder, name='recorder'),
+    url(r'^recorderWorker.js$', views.recorderWorker, name='recorderWorker'),
 ]
--- Interviewer/views.py
+++ Interviewer/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Interviewer/index.html')
+
+def recorder(request):
+    return render(request, 'Interviewer/recorder.html')
+
+def recorderWorker(request):
+    return render(request, 'Interviewer/recorderWorker.js')
